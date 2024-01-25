f11c2416c678fcf1f9cbcb4306e0a48227e589a2
Matteo Pierucci <matteo.pierucci4@gmail.com>
2024-01-25 01:06:02 +0100

    Added models, first view, form and template

---
+++ ServerPrototypeApp/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Ingredient(models.Model):
+    _ingredient_id = models.AutoField(primary_key=True)
+    _name = models.CharField(max_length=100)
+
+    @property
+    def ingredient_id(self):
+        return self._ingredient_id
+
+    @property
+    def name(self):
+        return self._name
+    
+
+class Recipe(models.Model):
+    _recipe_id = models.AutoField(primary_key=True)
+    _title = models.CharField(max_length=200)
+    _ingredients = models.ManyToManyField(Ingredient)
+    _instructions = models.TextField()
+    _restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
+
+    @property
+    def recipe_id(self):
+        return self._recipe_id
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def ingredients(self):
+        return self._ingredients.all()
+
+    @property
+    def instructions(self):
+        return self._instructions
+
+    @property
+    def restaurant(self):
+        return self._restaurant
+    
+
+class Restaurant(models.Model):
+    _restaurant_id = models.AutoField(primary_key=True)
+    _name = models.CharField(max_length=200)
+    _location = models.CharField(max_length=255)
+
+    @property
+    def restaurant_id(self):
+        return self._restaurant_id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def location(self):
+        return self._location
+
